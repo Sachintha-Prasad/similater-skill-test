ee4f613c8e419e6e6204a6895ed928eeb0351bff
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-08-03 16:58:44 +0530

    responsive navbar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-// src/components/Navbar.tsx
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { createPortal } from 'react-dom'
@@ -6,6 +5,8 @@ import logo from '../assets/similater long logo 1.png'
 import LoginModal from './LoginModal'
 import { useAuth } from '../context/authContext'
 import { toast } from 'react-toastify'
+import { FaBars } from 'react-icons/fa6'
+import { IoClose } from 'react-icons/io5'
 
 type NavLink = {
     name: string
@@ -21,6 +22,7 @@ const navLinks: NavLink[] = [
 
 const Navbar: React.FC = () => {
     const [showLoginModal, setShowLoginModal] = useState<boolean>(false)
+    const [menuOpen, setMenuOpen] = useState<boolean>(false)
     const { isAuthenticated, logout } = useAuth()
 
     const handleLoginModal = () => {
@@ -35,6 +37,10 @@ const Navbar: React.FC = () => {
             toast.error('Logout failed. Please try again.')
             console.error('Logout failed:', error)
         }
+    }
+
+    const toggleMenu = () => {
+        setMenuOpen((prevState) => !prevState)
     }
 
     return (
@@ -72,8 +78,29 @@ const Navbar: React.FC = () => {
                             Login
                         </button>
                     )}
+
+                    <div className="flex items-center text-lg sm:hidden">
+                        <button onClick={toggleMenu} className="menu-btn">
+                            {menuOpen ? <IoClose /> : <FaBars />}
+                        </button>
+                    </div>
                 </div>
             </nav>
+
+            {menuOpen && (
+                <div className="flex flex-col items-start gap-4 bg-primary-dark p-4 text-white sm:hidden">
+                    {navLinks.map((link, index) => (
+                        <Link
+                            key={index}
+                            to={link.path}
+                            className="custom-transition capitalize hover:text-gray-500 focus:text-gray-500"
+                            onClick={() => setMenuOpen(false)}
+                        >
+                            {link.name}
+                        </Link>
+                    ))}
+                </div>
+            )}
 
             {showLoginModal &&
                 createPortal(
